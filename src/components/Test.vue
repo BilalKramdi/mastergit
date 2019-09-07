6a1e4718bdd0335af1650a26d3dd8deba89b678d
Bilal <bilal.kramdi@gmail.com>
2019-09-07 12:30:22 +0200

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>Je vais bien</p>
+  <h1>Ok tout le monde va bien</h1>
+</template>
+
+<script>
+
+    }
+    export default {
+        name: "Test"
+    }
+</script>
+
+<style scoped>
+
+</style>
